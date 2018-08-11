0cbd86adfa94ead68a5875d53acd618557996255
Jakob <jakobabfalter@gmail.com>
2018-08-11 20:37:28 +0200

    Initial commit with implemented Password utitlity

---
+++ bin/cmd.js
@@ -0,0 +1,21 @@
+const CommandFactory = require('../lib/CommandFactory');
+
+const main = async () => {
+    if( process.argv.length < 3 ) { throw  new Error("At least one argument is expected") }
+    else {
+        const cmdtype = process.argv[2];
+        const args = [];
+        for( let i = 3; i < process.argv.length; i++ ) {
+            const a = process.argv[i];
+            args.push(a);
+        }
+        const cmd = CommandFactory.generateCommand(cmdtype, args);
+        return await cmd.execute();
+    }
+};
+
+main().then((r) => {
+    console.log(r);
+}).catch( e => {
+    console.error("An error has occured " + e.message);
+});

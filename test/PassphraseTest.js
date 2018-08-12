22ff4c8cfd2dc42af765b1df7510e2cc886ace93
Jakob <jakobabfalter@gmail.com>
2018-08-12 18:26:13 +0200

    German passphrase generator implemented and tested

---
+++ test/PassphraseTest.js
@@ -0,0 +1,52 @@
+const assert = require('assert');
+
+describe('Tests for the Passphrase Command', () => {
+
+    it('Should execute a Passphrase Command but fail due to non integer length argument', async () => {
+        const mockv = [
+            "node.exe",
+            "cmd.js",
+            "passphrase",
+            "dogecoin"
+        ];
+        process.argv = mockv;
+        const result = await main();
+        assert(result.startsWith('USAGE'));
+    });
+
+    it('Should execute a Passphrase Command but fail due to no length argument present', async () => {
+        const mockv = [
+            "node.exe",
+            "cmd.js",
+            "passphrase",
+        ];
+        process.argv = mockv;
+        const result = await main();
+        assert(result.startsWith('USAGE'));
+    });
+
+    it('Should execute a Passphrase Command but fail due to to much arguments specified', async () => {
+        const mockv = [
+            "node.exe",
+            "cmd.js",
+            "passphrase",
+            "2",
+            "Dogecoin"
+        ];
+        process.argv = mockv;
+        const result = await main();
+        assert(result.startsWith('USAGE'));
+    });
+
+    it('Should execute a Passphrase Command and return a valid passphrase with 5 words', async () => {
+        const mockv = [
+            "node.exe",
+            "cmd.js",
+            "passphrase",
+            "5"
+        ];
+        process.argv = mockv;
+        const result = await main();
+        assert(result.length > 0);
+    });
+});
